d9509c0d3c56464375e05292b4aea5733b86035a
Sandra Yun <sandramayla@gmail.com>
2022-08-30 18:51:12 -0400

    commit tree 45659778e0bec954d19f0405d597915b8629ac9e

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+const baseURL = "https://dog.ceo/api/breeds/image/random"
+
+function App() {
+    const [message, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch(baseURL)
+            .then (resp => resp.json())
+            .then (data => setImage(data.message))
+    }, [])
+    if(!message) {
+        return <p>"Loading..."</p> // conditional rendering
+    }
+    return (
+        <div>
+        <p>Dog Ceo API</p>
+        <img src={message} alt="A Random Dog" />
+        </div>
+    );
+}
+
+export default App; 
